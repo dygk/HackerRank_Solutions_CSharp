434f9af5a0624a1f745b929fbc97a02b70be9db5
Duygu Kayabasi <kayabasiduygu@gmail.com>
2021-03-31 23:17:32 +0300

    Repeated String Solution

---
+++ RepeatedString/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+public class Solution
+
+{
+    static long repeatedString(string s, long n)
+    {
+        long result = 0;
+        if (s.Contains('a'))
+        {
+            char ch = 'a';
+            long stringLength = s.Length;
+            int freqA = Regex.Matches(s, ch.ToString()).Count;
+            int remainderValue = (int)(n % stringLength);
+
+            result = (n / stringLength) * freqA;
+
+            if (remainderValue != 0)
+                result += Regex.Matches(s.Substring(0, remainderValue), ch.ToString()).Count;
+
+        }
+
+        return result;
+    }
+    static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string s = Console.ReadLine();
+
+        long n = Convert.ToInt64(Console.ReadLine());
+
+        long result = repeatedString(s, n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
