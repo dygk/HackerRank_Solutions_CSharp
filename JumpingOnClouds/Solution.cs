a1587d568ca023fb92e3bf8c6d5573b25b3bc06e
Duygu Kayabasi <kayabasiduygu@gmail.com>
2021-04-01 23:48:06 +0300

    Jumping On Clouds

---
+++ JumpingOnClouds/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Solutions_CSharp.JumpingOnClouds
+{
+    class Solution
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string cloudsArray = Console.ReadLine();
+
+
+        }
+    }
+}
