dfaa6ac782d9f25c21437740b9c04432677895a1
Duygu Kayabasi <kayabasiduygu@gmail.com>
2021-04-03 23:34:36 +0300

    MinMaxSum

---
+++ MiniMaxSum/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Solutions_CSharp.MiniMaxSum
+{
+    class Solution
+    {
+
+        // Complete the miniMaxSum function below.
+        static void miniMaxSum(int[] arr)
+        {
+            long minSum = 0;
+            long maxSum = 0;
+
+            Array.Sort(arr);
+
+            minSum = arr.AsParallel().Sum(x=> (long)x) - arr[4];
+
+            Array.Reverse(arr);
+
+            maxSum = arr.AsParallel().Sum(x => (long)x) - arr[4];
+
+            Console.WriteLine(minSum + "  " + maxSum);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            ;
+            miniMaxSum(arr);
+        }
+
+
+
+    }
+}
