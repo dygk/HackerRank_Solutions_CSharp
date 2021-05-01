bde325654527e06983bf7de86407af5b6edfc54b
Duygu Kayabasi <kayabasiduygu@gmail.com>
2021-05-01 22:35:56 +0300

    Replace All Digits

---
+++ HackerRank_Solutions_CSharp.Tests/ReplaceAllDigitsWithCharsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HackerRank_Solutions_CSharp.Tests
+{
+    public class ReplaceAllDigitsWithCharsTest
+    {
+        [Theory]
+        [InlineData("a1c1e1", "abcdef")]
+
+        public void ComputesCorrectResponse(string c,  string correctResult)
+        {
+
+            Assert.Equal( ReplaceAllDigitsWithChars.ReplaceAllDigitsWithChars.ReplaceDigits(c), correctResult);
+        }
+    }
+}
---
+++ HackerRank_Solutions_CSharp/ReplaceAllDigitsWithChars/ReplaceAllDigitsWithChars.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank_Solutions_CSharp.ReplaceAllDigitsWithChars
+{
+    public class ReplaceAllDigitsWithChars
+    {
+        public static string ReplaceDigits(string s)
+        {
+
+            char[] str = s.ToCharArray();
+
+            for (int i = 1; i < str.Length; i = i + 2)
+            {
+
+                str[i] = Shift(str[i - 1], int.Parse(str[i].ToString()));
+
+            }
+            string result = new(str);
+            return result;
+
+        }
+
+        public static char Shift(char letter, int x)
+        {
+            char nextChar;
+            if (letter == 'z')
+                nextChar = 'z';
+            else if (letter == 'Z')
+                nextChar = 'Z';
+
+            else
+                nextChar = (char)((int)letter + x);
+
+            return nextChar;
+        }
+
+        static void Main(string[] args)
+        {
+            ReplaceDigits("a1c1e1");
+        }
+
+    }
+}
